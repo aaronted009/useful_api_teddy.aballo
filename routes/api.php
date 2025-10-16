6fb159dc7f0e15d4081755bbd4bf9a42a337b155
Teddy Aballo <teddy.aballo@epitech.eu>
2025-10-16 15:33:15 +0100

    feat: [auth middleware] apply auth sanctum middleware to routes except register and login

--- routes/api.php
+++ routes/api.php
@@ -6,11 +6,11 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('users', UserController::class)->except(['store']);
-
 Route::post('/register', [UserController::class, 'store']);
-
 Route::post('/login', [AuthController::class, 'login']);
-// Route::post('/logout', [AuthController::class, 'logout']);
 
-Route::get('/modules', [ModuleController::class, 'index']);
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('users', UserController::class)->except(['store']);
+    // Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/modules', [ModuleController::class, 'index']);
+});
