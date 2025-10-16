2c016a663c3c9dc8bce165f4fa1d0010d0c309a9
Teddy Aballo <teddy.aballo@epitech.eu>
2025-10-16 17:51:08 +0100

    feat: [validation] add validation on payload when creating a shorten link

---
+++ app/Http/Requests/StoreShortLinkRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreShortLinkRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'original_url' => 'required|url:http,https',
+            'custom_code' => 'max:10|unique:short_links,custom_code|alpha_num|alpha_dash:ascii',
+        ];
+    }
+}
