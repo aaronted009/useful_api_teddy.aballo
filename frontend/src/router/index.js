3acf9d3626b44f4317f1c55800cddb77cc3eb0af
Teddy Aballo <teddy.aballo@epitech.eu>
2025-10-17 11:23:19 +0100

    feat: [views] add register & login views

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
+import RegisterView from '@/views/RegisterView.vue'
+import LoginView from '@/views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,18 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+
+    {
+      path: '/auth/register',
+      name: 'register',
+      component: RegisterView,
+    },
+
+    {
+      path: '/auth/login',
+      name: 'login',
+      component: LoginView,
     },
   ],
 })
---
+++ frontend/src/views/RegisterView.vue
@@ -0,0 +1,8 @@
+<script setup>
+import HeaderComponent from '@/components/HeaderComponent.vue'
+</script>
+
+<template>
+  <HeaderComponent />
+  Here the register form
+</template>
